4b0ef3d7cabc03e1d6fcdbcf8fa62b953e9c5e1b
rehanulhb <rehanulh@gmail.com>
2024-10-13 03:21:38 +0200

    Check API testing using Function calling from index page

--- js/fetch.js
+++ js/fetch.js
@@ -1,3 +1,8 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+
+function loadData(){
+    fetch(url)
       .then(response => response.json())
       .then(json => console.log(json))
+}
